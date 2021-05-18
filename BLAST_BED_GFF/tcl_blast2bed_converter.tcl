0da1266ece4f77d52bf412cdf24191383bf3a90f
Alex Kozik <akozik@gmail.com>
2021-05-17 18:34:50 -0700

    Update tcl_blast2bed_converter.tcl

--- BLAST_BED_GFF/tcl_blast2bed_converter.tcl
+++ BLAST_BED_GFF/tcl_blast2bed_converter.tcl
@@ -25,37 +25,54 @@ proc Process_Tables { argv } {
 
     ####### READ BLAST ALL_HITS TABLE AND CONVERT TO BED #######
     set l 0
+    set blank_line_counter 0
     while { [gets $f_in1 current_line] >= 0 } {
-	set current_data [split   $current_line           "\t"]
-	set subj_id      [lindex  $current_data   $subj_column]
-	set coord1       [lindex  $current_data   $coord1_clmn]
-	set coord2       [lindex  $current_data   $coord2_clmn]
-
-	set coord1       [expr  $coord1 - $off_index]
-	set coord2       [expr  $coord2 - $off_index]
-
-	if {$coord1 < $coord2} {
-		### BED starts are zero-based and BED ends are one-based
-		### START ###
-		set coord1_bed [expr $coord1 - 1]
-		### END ###
-		set coord2_bed [expr $coord2 - 0]
-		puts $f_out "$subj_id\t$coord1_bed\t$coord2_bed"
+    
+    	set tab_data "TRUE"
+    
+    	if {$current_line == ""} {
+		set tab_data "FALSE"
+		incr blank_line_counter
 	}
-        if {$coord1 > $coord2} {
-		### BED starts are zero-based and BED ends are one-based
-		### END ###
-		set coord1_bed [expr $coord1 - 0]
-		### START ###
-		set coord2_bed [expr $coord2 - 1]
-                puts $f_out "$subj_id\t$coord2_bed\t$coord1_bed"
-        }
-        if {$coord1 == $coord2} {
-		puts        " + TOO GOOD TO BE TRUE + "
-                puts $f_out " + TOO GOOD TO BE TRUE + "
-		exit
-        }
-	incr l
+	
+    	set comment_line [string range $current_line 0 0]
+	if {$comment_line == "#"} {
+		set tab_data "FALSE"
+	}
+    
+    	if {$tab_data == "TRUE"} {
+	
+		set current_data [split   $current_line           "\t"]
+		set subj_id      [lindex  $current_data   $subj_column]
+		set coord1       [lindex  $current_data   $coord1_clmn]
+		set coord2       [lindex  $current_data   $coord2_clmn]
+
+		set coord1       [expr  $coord1 - $off_index]
+		set coord2       [expr  $coord2 - $off_index]
+
+		if {$coord1 < $coord2} {
+			### BED starts are zero-based and BED ends are one-based
+			### START ###
+			set coord1_bed [expr $coord1 - 1]
+			### END ###
+			set coord2_bed [expr $coord2 - 0]
+			puts $f_out "$subj_id\t$coord1_bed\t$coord2_bed"
+		}
+		if {$coord1 > $coord2} {
+			### BED starts are zero-based and BED ends are one-based
+			### END ###
+			set coord1_bed [expr $coord1 - 0]
+			### START ###
+			set coord2_bed [expr $coord2 - 1]
+			puts $f_out "$subj_id\t$coord2_bed\t$coord1_bed"
+		}
+		if {$coord1 == $coord2} {
+			puts        " + TOO GOOD TO BE TRUE + "
+			puts $f_out " + TOO GOOD TO BE TRUE + "
+			exit
+		}
+		incr l
+	}
     }
     close $f_in1
     close $f_out
